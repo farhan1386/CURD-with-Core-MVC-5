c6f326dc912daad65349b5ac152df5f8a0173d84
Farhan Ahmed <farhan1086@gmail.com>
2023-12-02 14:11:41 +0530

    Rename files

---
+++ CURDOperationWithImageUploadCore5_Demo/ViewModels/FileEditViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CURDOperationWithImageUploadCore5_Demo.ViewModels
+{
+    public class FileEditViewModel : FileUploadViewModel
+    {
+        public int Id { get; set; }
+        public string ExistingImage { get; set; }
+    }
+}
